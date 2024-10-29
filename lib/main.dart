29ec63fa290617cb563f923738a35f4ef7aaa0fe
Alfonzo <thulanikgakole@gmail.com>
2024-10-29 21:06:42 +0200

    done setting up the firebase database and other this that will work for the backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:instragram_clone/responsive/mobileScreenLayout.dart';
 import 'package:instragram_clone/responsive/responsiveLayout.dart';
 import 'package:instragram_clone/responsive/webScreenLayout.dart';
 import 'package:instragram_clone/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -14,13 +17,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Instagram Clone',
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: mobileBackgroundColor
-      ),
-      home: const Responsivelayout(webScreenLayout: Webscreenlayout(), mobileScreenLayout: Mobilescreenlayout(),)
-    );
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Instagram Clone',
+        theme: ThemeData.dark()
+            .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
+        home: const Responsivelayout(
+          webScreenLayout: Webscreenlayout(),
+          mobileScreenLayout: Mobilescreenlayout(),
+        ));
   }
 }
